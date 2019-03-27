69a06f089455d73ed8c50b7fabb75a5040656b9c
Phil Chen <winkpoke@yahoo.com>
2019-03-27 22:33:53 +0800

    add bash/zsh init files

--- delopy.sh
+++ delopy.sh
@@ -7,3 +7,6 @@ ln -s ${DIR}/init.vim ~/.config/nvim/init.vim
 
 ln -s ${DIR}/tmux/tmux.conf ~/.tmux.conf
 ln -s ${DIR}/tmux/.tmux ~/.tmux
+
+ln -s ${DIR}/zshrc ~/.zshrc
+ln -s ${DIR}/bashrc ~/.bashrc
