ed8d1e2d6c6f19a43fa057516baba7c023c143c3
Phil Chen <winkpoke@yahoo.com>
2019-03-30 11:18:52 +0800

    bug fixes in deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+DIR=~/dotfiles
+LN="ln -s -b -f" 
+
+${LN} ${DIR}/init.vim ~/.vimrc
+mkdir -p ~/.config/nvim
+${LN} ${DIR}/init.vim ~/.config/nvim/init.vim
+mkdir -p ~/.vim/autoload
+cp ${DIR}/pathogen.vim ~/.vim/autoload
+cp ${DIR}/plug.vim ~/.vim/autoload
+
+${LN} ${DIR}/tmux/tmux.conf ~/.tmux.conf
+${LN} ${DIR}/tmux/.tmux ~/.tmux
+
+${LN} ${DIR}/zshrc ~/.zshrc
+${LN} ${DIR}/bashrc ~/.bashrc
+
+${LN} ${DIR}/xprofile ~/.xprofile
